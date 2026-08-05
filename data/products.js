27a41fa1ec3d6dcaaaff29a7fde56d62389774c8
baoqk1online <baoqk1online@gmail.com>
2026-08-05 10:43:25 +0700

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,33 @@
+const products = [
+
+    {
+        id: "newspaper",
+
+        title: "Số báo đặc biệt kỷ niệm 80 năm ra số báo đầu tiên (19/08/1946-19/08/2026)",
+
+        url: "",
+
+        thumbnail: "assets/products/newspaper.jpg"
+    },
+
+    {
+        id: "documentary",
+
+        title: "Phim truyền thống 80 năm Báo Quân khu 1",
+
+        url: "",
+
+        thumbnail: "assets/products/documentary.jpg"
+    },
+
+    {
+        id: "website",
+
+        title: "Trang Báo Quân khu 1 Điện tử",
+
+        url: "https://baoquankhu1.vn/",
+
+        thumbnail: "assets/products/website.jpg"
+    }
+
+];

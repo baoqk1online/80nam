2b24013c2450154022b96bcc2ab6031fcfc52848
baoqk1online <baoqk1online@gmail.com>
2026-08-05 10:47:44 +0700

    Create timeline.js

---
+++ data/timeline.js
@@ -0,0 +1,31 @@
+const timeline = [
+
+    {
+        year: "1946",
+
+        title: "CHIẾN KHU",
+
+        subtitle: "Khởi nguồn của tờ báo",
+
+        description:
+
+            "Số báo đầu tiên ra đời giữa núi rừng Việt Bắc.",
+
+        theme: "old",
+
+        side: "left",
+
+        background:
+
+            "assets/timeline/1946.jpg",
+
+        images: [
+
+            "assets/timeline/1946-1.jpg",
+
+            "assets/timeline/1946-2.jpg"
+
+        ]
+    }
+
+];

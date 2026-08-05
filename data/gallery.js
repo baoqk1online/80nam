249a135ae04308b97cca169dfec1ed50fa59d176
baoqk1online <baoqk1online@gmail.com>
2026-08-05 22:20:33 +0700

    Create gallery.js

---
+++ data/gallery.js
@@ -0,0 +1,143 @@
+/* =======================================
+
+TRIỂN LÃM ẢNH
+
+Chỉ sửa:
+
+- title
+- caption
+- image
+- link
+- type
+
+type:
+
+"landscape" = ảnh ngang
+
+"portrait" = ảnh dọc
+
+======================================= */
+
+const gallery = [
+
+    {
+
+        title: "Tòa soạn qua các thời kỳ",
+
+        caption:
+
+            "Không gian làm việc của Báo Quân khu 1.",
+
+        image:
+
+            "assets/gallery/01.jpg",
+
+        link:
+
+            "assets/gallery/01-large.jpg",
+
+        type: "landscape"
+
+    },
+
+    {
+
+        title: "Báo chí trên thao trường",
+
+        caption:
+
+            "Phóng viên tác nghiệp tại đơn vị.",
+
+        image:
+
+            "assets/gallery/02.jpg",
+
+        link:
+
+            "assets/gallery/02-large.jpg",
+
+        type: "landscape"
+
+    },
+
+    {
+
+        title: "Những số báo đặc biệt",
+
+        caption:
+
+            "Các ấn phẩm tiêu biểu.",
+
+        image:
+
+            "assets/gallery/03.jpg",
+
+        link:
+
+            "assets/gallery/03-large.jpg",
+
+        type: "portrait"
+
+    },
+
+    {
+
+        title: "Lễ kỷ niệm",
+
+        caption:
+
+            "Hoạt động chào mừng 80 năm.",
+
+        image:
+
+            "assets/gallery/04.jpg",
+
+        link:
+
+            "assets/gallery/04-large.jpg",
+
+        type: "landscape"
+
+    },
+
+    {
+
+        title: "Phóng viên hiện trường",
+
+        caption:
+
+            "Tác nghiệp trong điều kiện dã ngoại.",
+
+        image:
+
+            "assets/gallery/05.jpg",
+
+        link:
+
+            "assets/gallery/05-large.jpg",
+
+        type: "landscape"
+
+    },
+
+    {
+
+        title: "Báo Quân khu 1 hôm nay",
+
+        caption:
+
+            "Cơ quan báo chí đa phương tiện.",
+
+        image:
+
+            "assets/gallery/06.jpg",
+
+        link:
+
+            "assets/gallery/06-large.jpg",
+
+        type: "landscape"
+
+    }
+
+];

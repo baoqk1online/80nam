d80b6ad224f700d0a953880b48f7a76349b757e2
baoqk1online <baoqk1online@gmail.com>
2026-08-06 23:01:15 +0700

    Update social.js

--- data/social.js
+++ data/social.js
@@ -1,43 +1,45 @@
-const social = [
+window.social = [
 
     {
-        platform: "website",
 
-        title: "Báo điện tử",
-
-        url: "https://baoquankhu1.vn/",
-
-        thumbnail: "assets/products/website.png"
-    },
-
-    {
-        platform: "facebook",
+        id: "facebook",
 
         title: "Facebook",
+
+        subtitle: "Báo Quân khu 1 Điện tử",
 
         url: "https://www.facebook.com/baoquankhu1",
 
         thumbnail: "assets/products/facebook.png"
+
     },
 
     {
-        platform: "youtube",
+
+        id: "youtube",
 
         title: "YouTube",
+
+        subtitle: "Báo - Truyền hình Quân khu 1",
 
         url: "https://www.youtube.com/@bao-truyenhinhquankhu1",
 
         thumbnail: "assets/products/youtube.png"
+
     },
 
     {
-        platform: "tiktok",
+
+        id: "tiktok",
 
         title: "TikTok",
+
+        subtitle: "Báo Quân khu 1",
 
         url: "https://www.tiktok.com/@baoquankhu1",
 
         thumbnail: "assets/products/tiktok.png"
+
     }
 
 ];
